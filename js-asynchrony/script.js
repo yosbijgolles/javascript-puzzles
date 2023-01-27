c9fc8a52019c17c0b53cf251e67b73351d918656
Yosbi J. Golles <ysbglls@gmail.com>
2023-01-26 19:44:38 -0500

    Soluciones asynchrony

--- js-asynchrony/script.js
+++ js-asynchrony/script.js
@@ -68,3 +68,64 @@ console.log("Mario");
 setTimeout(() => {
   console.log("Martin");
 }, 1);
+
+// 2) Utilizando setTimeout, crear una funcion que
+// actue como un temporizador, haciendo una cuenta
+// regresiva desde el numero de segundos que se pase
+// como argumentos hasta llegar a 0. Para ese ejercicio
+// no se puede utilizar ningun tipo de loop (for, while, etc)
+//PISTA: Para resolver el ejerciio, pueden investigar un
+// poco sobre funciones recursivas.
+
+const cuentatras = (n) => {
+  if (n < 0) return;
+  console.log(n);
+  setTimeout(() => {
+    cuentatras(n - 1);
+  }, 1000);
+};
+
+cuentatras(8);
+
+//3) Utilizando la API de GITHUB (https://api.github.com)
+//realizar las siguientes tareas
+
+// a) Obtener una lista de los repositorios publicos
+// (Documentacion de la API: https://docs.github.com/es/rest/reference/repos#list-public-repositories)
+
+const obtenerRepositorios = async () => {
+  resultado = await fetch("https://api.github.com/repositories");
+  resultadojs = await resultado.json();
+  return resultadojs;
+};
+
+obtenerRepositorios();
+
+// b) Generar una funcion que a partir de la respuesta
+// del punto anterior, retorne un array de los nombres
+// de usuario de los owners de los repositorios listados
+
+const obtenerNombres = async () => {
+  const repositorios = await obtenerRepositorios();
+  const nombres = repositorios.map((x) => x.owner.login);
+  console.log(nombres);
+};
+
+obtenerNombres();
+
+//c) Tomar de la respuesta al primer respositorio, y
+// listar todos los repositorios del owner.
+// PISTA: Para ver el endpoint al cual deberas pegarle
+// para traer los repositorios del owner, deberas
+// inspeccionar la informacion que recibes dentro del
+// primer request.
+
+const obtenerRepositoriosOwner = async () => {
+  const repositorios = await obtenerRepositorios();
+  const endpoint = repositorios[0].owner.repos_url;
+  const request = await fetch(endpoint);
+  const requestjs = await request.json();
+  console.log(requestjs);
+};
+
+obtenerRepositoriosOwner();
